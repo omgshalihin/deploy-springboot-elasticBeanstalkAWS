238e1aaa7bc76edfa9435711e3721fee8f25ff29
omgshalihin <shalihinsgp@gmail.com>
2023-02-09 11:45:34 +0100

    mvn clean package ran successfully

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,4 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/welcome")
 public class WelcomeController {
+
+
+    @GetMapping
+    public static String welcome() {
+        return "Welcome!!!";
+    }
 }
